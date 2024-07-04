7eab62e7b908727e9f64d5077696f18a0c739dc5
Aneta <=>
2024-07-04 16:00:16 +0200

    pbieranie danych

---
+++ Python3/funkcje.py
@@ -0,0 +1,26 @@
+
+def print_end():
+    print("END")
+
+
+
+def calculate_to_5():
+    print("1")
+    print("2")
+    print("3")
+    print("4")
+    print("5")
+
+def say_hello(name):
+    print("Siemka ", name)
+
+
+def return_5():
+    return 5
+
+
+calculate_to_5()
+print_end()
+say_hello("Aneta")
+a = return_5()
+print("A to ", a)
---
+++ Python3/zad5.py
@@ -0,0 +1,38 @@
+# Msc zer funkcji 
+a = 0
+b = 0
+c = 0
+
+def load_data(a,b,c):
+    a=int(input("podaj parametr a \n"))
+    b=int(input("podaj parametr b \n"))
+    c=int(input("podaj paraetr c \n"))
+
+    # todo
+
+def calculate_delta(a,b,c):
+    delta = 0
+    return delta
+
+def calucate_x1(b,delta,a):
+    pass
+
+def calucate_x2(b,delta,a):
+    pass
+
+def show_results(x1, x2):
+    pass
+
+
+#1podaj wspolczynniki ruwnania kwadratowego...
+load_data(a,b,c)
+
+#2 oblicz delte
+delta = calculate_delta(a,b,c)
+#3 oblicz pierwsze miejsce zerowe e
+x1=calucate_x1(b, delta,a)
+
+#4 oblicz drugie miejsce zerow
+x2=calucate_x2(b,delta,a)
+#5 wypisz wyniki
+show_results(x1, x2)
